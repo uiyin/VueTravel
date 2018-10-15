c99e18c495a33834358241cbe144c027097b461e
trigger <triggersshow@gmail.com>
2018-10-15 15:11:46 +0800

    利用递归完成详情页的布局

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <DetailBanner></DetailBanner>
     <Header></Header>
+    <DetailList :list="list"></DetailList>
     <div class="content"></div>
   </div>
 </template>
@@ -9,16 +10,35 @@
 <script>
 import DetailBanner from './components/banner.vue'
 import Header from './components/Header.vue'
+import DetailList from './components/List.vue'
 export default {
   name: 'Detail',
   data () {
     return {
-      message: '详情'
+      message: '详情',
+      list: [{
+        'title': '成人票',
+        'children': [{
+          'title': '成人三馆联票',
+          'children': [{
+            'title': '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          'title': '成人五馆联票'
+        }]
+      }, {
+        'title': '学生票'
+      }, {
+        'title': '儿童票'
+      }, {
+        'title': '特惠票'
+      }]
     }
   },
   components: {
     DetailBanner,
-    Header
+    Header,
+    DetailList
   }
 }
 </script>
---
+++ src/pages/Detail/components/List.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="item"
+         v-for="(item,index) in list"
+         :key="index">
+      <div class="iconfont icon-jiantouxia weizhi"></div>
+      <div class="itemtitle">{{item.title}}</div>
+      <div v-if="item.children"
+           class="leftindent">
+        <DetailList :list="item.children"></DetailList>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: ['list']
+}
+</script>
+
+<style scoped lang="less">
+@import '~styles/common.less';
+.item {
+  .weizhi {
+    font-size: 14px;
+    float: left;
+    line-height: 0.6rem;
+    margin-right: 0.2rem;
+    color: @bgcolor;
+    margin-left: 0.2rem;
+  }
+  .itemtitle {
+    width: 100%;
+    line-height: 0.6rem;
+    border-bottom: 1px solid #ccc;
+    font-size: 16px;
+    color: @bgcolor;
+  }
+  .leftindent {
+    .weizhi {
+      text-indent: 0.2rem;
+    }
+  }
+}
+</style>
